622d1bc0e403c30190cd66e57f8fe38418722ab0
geoffrey521 <calvino.geoffrey@gmail.com>
2021-09-29 12:40:18 +0200

    feature / 1 - initialize project in progress

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 * La thématique du commit est indiquée entre crochets
 * Suivit du type de travail (evol, fix, update)
 * Une description courte du contenu précédé d'un séparateur "-" termine le commit
+* git commit -m"Feature #1200 : Inscription user"
 
 # Installation du projet
--- web/index.php
+++ web/index.php
@@ -1,6 +1,10 @@
 <?php
+require_once '../vendor/autoload.php';
 
-//phpinfo();
+$loader = new \Twig\Loader\ArrayLoader([
+   'index' => 'hello {{ name }}!',
+]);
+$twig = new \Twig\Environment($loader);
 
 ?>
 
@@ -11,10 +15,47 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Freelancer - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="public/css/freelancer.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-    <h1>Hello</h1>
+    <h1><?= $twig->render('index', ['name' => 'Twig']); ?></h1>
+    <p>
+        <?php
+        $hostname = 'db';
+        $username = 'root';
+        $password = 'root';
+        $db = 'blogphp';
+
+        try{
+            $conn = new PDO("mysql:host=$hostname;dbname=$db", $username, $password);
+            //On définit le mode d'erreur de PDO sur Exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo 'Connexion réussie';
+        }
+
+        catch(PDOException $e){
+            echo "Erreur : " . $e->getMessage();
+        }
+
+        ?>
+    </p>
 </body>
 </html>
-
