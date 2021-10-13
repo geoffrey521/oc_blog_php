59ea8868aefa001fb798801db22635428c222279
geoffrey521 <calvino.geoffrey@gmail.com>
2021-10-13 22:34:47 +0200

    feature / 2 - base setup - import template files and configure twig

--- app/public/index.php
+++ app/public/index.php
@@ -1,60 +1,36 @@
 <?php
-require_once '../../vendor/autoload.php';
 
-$loader = new \Twig\Loader\ArrayLoader([
+use App\Controller\MainController;
+
+require '../../vendor/autoload.php';
+
+$frontController = new MainController();
+
+/*$loader = new \Twig\Loader\ArrayLoader([
    'index' => 'hello {{ name }}!',
+]);*/
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../src/Views');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
 ]);
-$twig = new \Twig\Environment($loader);
 
-?>
+$page = '';
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Freelancer - Start Bootstrap Theme</title>
+if (isset($_GET['p'])) {
+    $page = $_GET['p'];
+}
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Theme CSS -->
-    <link href="css/freelancer.min.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic"
-          rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-    <h1><?= $twig->render('index', ['name' => 'Twig']); ?></h1>
-    <p>
-        <?php
-        $hostname = 'db';
-        $username = 'root';
-        $password = 'root';
-        $db = 'blogphp';
-
-        try {
-            $conn = new PDO("mysql:host=$hostname;dbname=$db", $username, $password);
-            //On définit le mode d'erreur de PDO sur Exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo 'Connexion réussie';
-        } catch (PDOException $e) {
-            echo "Erreur : " . $e->getMessage();
-        }
-
-        ?>
-    </p>
-</body>
-</html>
+switch ($page) {
+    case 'home':
+        echo $twig->render('content/node/homepage.html.twig');
+        break;
+    case 'test':
+        echo $twig->render('content/node/test.html.twig', ['person' => [
+            'name' => 'George',
+            'age' => 22
+        ]]);
+        break;
+    default:
+        echo $twig->render('pages/page-404.html.twig');
+        break;
+}
---
+++ app/src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+class MainController
+{
+
+ /*   protected $twig = null;
+
+    public function __construct()
+    {
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../Views');
+        $this->twig = new \Twig\Environment($loader, [
+            'cache' => false
+        ]);
+    }*/
+}
