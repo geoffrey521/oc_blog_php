03780fb0b666bc3fff9ffb40d2fc26cf494ce112
geoffrey521 <calvino.geoffrey@gmail.com>
2021-12-01 18:58:42 +0100

    fix / 46 - finalize - update app/bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -11,15 +11,3 @@ const MAX_IMAGE_POST_FILESIZE = 1000000;
 
 define("DEBUG_TIME", microtime(true));
 
-function underscoreToCamelCase($string, $capitalizeFirstCharacter = false)
-{
-
-    $str = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
-
-    if (!$capitalizeFirstCharacter) {
-        $str[0] = strtolower($str[0]);
-    }
-
-    return $str;
-}
-
